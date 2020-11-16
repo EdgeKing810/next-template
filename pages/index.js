e70ae1f50395b7807d71d51eaa36bbe281182e73
Kishan Takoordyal <kishan@kinesis.games>
2020-11-16 07:05:32 +0400

    init

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div className="w-screen h-screen overflow-hidden bg-gray-300">
+      <div className="w-full h-full flex justify-center items-center sm:text-6xl text-4xl font-mono font-bold tracking-wider text-blue-800">
+        Hello World
+      </div>
+    </div>
+  );
+}
